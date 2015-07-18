01aa78a23ab59803a12f9c82f149f5a6740d66b7
Ed Sutter <edsutterjr@gmail.com>
2015-07-18 11:27:12 -0400

    INCLUDE_MEMCMDS eliminates the need for INCLUDE_DM/PM

--- ports/beagleboneblack/config.h
+++ ports/beagleboneblack/config.h
@@ -152,8 +152,6 @@
 						
 #define INCLUDE_MEMTRACE	0
 #define INCLUDE_MEMCMDS         1
-#define INCLUDE_DM				1
-#define INCLUDE_PM				1
 #define INCLUDE_EDIT            0
 #define INCLUDE_DISASSEMBLER    0
 #define INCLUDE_UNZIP           0
