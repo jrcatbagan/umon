7d57a0a5c04e5cb32ffc6f93c59db036a0a1b088
Jarielle Catbagan <jcatbagan93@gmail.com>
2015-08-06 21:41:36 -0700

    BBB: am335x_ethernet.c: Add base version of function definitions

--- ports/beagleboneblack/am335x_ethernet.c
+++ ports/beagleboneblack/am335x_ethernet.c
@@ -5,3 +5,70 @@
  * found in the file LICENSE in this distribution or at
  * http://www.apache.org/licenses/LICENSE-2.0
  */
+
+unsigned char *
+getXmitBuffer(void)
+{
+	return(0);
+}
+
+int
+sendBuffer(int length)
+{
+	return(0);
+}
+
+void
+disablePromiscuousReception(void)
+{
+}
+
+int enselftest(int verbose)
+{
+	return(0);
+}
+
+void
+enreset(void)
+{
+}
+
+void
+enablePromiscuousReception(void)
+{
+}
+
+void
+ShowEtherdevStats(void)
+{
+}
+void
+DisableEtherdev(void)
+{
+}
+
+int
+EtherdevStartup(int verbose)
+{
+	return(0);
+}
+
+int
+polletherdev(void)
+{
+	return(0);
+}
+
+char *
+extGetIpAdd(void)
+{
+	return(0);
+}
+
+char *
+extGetEtherAdd(void)
+{
+	return(0);
+}
+
+
